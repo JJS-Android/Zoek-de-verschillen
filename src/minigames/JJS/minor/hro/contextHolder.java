2cb2dfe1ee634122349354a98aff24c3d39a774d
sanderpott <beedeeboy@gmail.com>
2012-10-02 13:07:49 +0200

    Initial commit
    Initial commit

---
+++ src/minigames/JJS/minor/hro/contextHolder.java
@@ -0,0 +1,31 @@
+package minigames.JJS.minor.hro;
+
+import android.app.Activity;
+
+public class contextHolder{
+
+	private static contextHolder _ch = null;
+	private Activity _c;
+	
+	private contextHolder(){
+		
+	}
+	
+	public static contextHolder getInstance(){
+		if(_ch == null){
+			_ch = new contextHolder();
+		}
+		
+		return _ch;
+	}
+	
+	public void setContext(Activity c){
+		if(_c == null){
+			_c = c;
+		}
+	}
+	
+	public Activity getContext(){
+		return _c;
+	}
+}
